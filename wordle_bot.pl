7e238401a5e7ca26726e44e882ca6a4ea05f94f8
Marcus Toma <best4tdm@gmail.com>
2022-02-05 16:44:37 +0100

    optimisation & fixed double letters & multiple calls

--- wordle_bot.pl
+++ wordle_bot.pl
@@ -1,14 +1,21 @@
-:- dynamic generationWords/1, guessWords/1, generatedWord/1, guessCount/1, orangeLetter/2, greenLetter/2, greyLetter/1, wordFrequency/2, wordScore/2, gen/2.
+:- dynamic runs/1, generationWords/1, guessWords/1, generatedWord/1, guessCount/1, orangeLetter/2, greenLetter/2, greyLetter/1, wordFrequency/2, wordScore/2, gen/2.
 
-isGenWord(Word) :- gen(Word, _).
-isGuessWord(Word) :- wordFrequency(Word, _).
+% ----------------------------------------------------------------------------------------------------------------- %
+% Global predicates ----------------------------------------------------------------------------------------------- %
+% ----------------------------------------------------------------------------------------------------------------- %
 assert_once(Fact) :- \+(Fact), !, assert(Fact).
 assert_once(_).
+isGenWord(Word) :- gen(Word, _).
+isGuessWord(Word) :- wordFrequency(Word, _).
+isRelevantWord(Word) :- wordScore(Word, _).
+increment_count :- guessCount(Count), retractall(guessCount(_)), NewCount is Count + 1, assert(guessCount(NewCount)).
+increment_runs :- runs(N), retractall(runs(_)), NewN is N + 1, assert(runs(NewN)).
+update_possible_guesses_list(List) :- retractall(guessWords(_)), assert(guessWords(List)).
 
 % ----------------------------------------------------------------------------------------------------------------- %
-% Load databases into the program
+% Import databases into the program ------------------------------------------------------------------------------- %
 % ----------------------------------------------------------------------------------------------------------------- %
-load :- 
+import :- 
     retractall(generationWords(_)), retractall(guessWords(_)),
     cd('C:/Users/DT Mark/Documents/Game Development/Projects/Wordle Bot'),
     csv_read_file('generation_table.csv', GenerationRows, [functor(gen), arity(2)]), maplist(assert, GenerationRows),
@@ -18,9 +25,35 @@ load :-
     findall(Word, isGuessWord(Word), GuessWords), assert(guessWords(GuessWords)).
 
 % ----------------------------------------------------------------------------------------------------------------- %
-% Initialise a game by resetting global variables, generating a word, and launching guesses
+% Export data to csv file ----------------------------------------------------------------------------------------- %
 % ----------------------------------------------------------------------------------------------------------------- %
-launch :- 
+export :- 
+    generatedWord(GeneratedWord), guessCount(C),
+    get_time(TimeStamp), append('output_', TimeStamp, OutFileName), append(OutFileName, '.csv', OutFile),
+    csv_write_stream(OutFile, [row(GeneratedWord, C)], []).
+
+% ----------------------------------------------------------------------------------------------------------------- %
+% Launch a batch of games ----------------------------------------------------------------------------------------- %
+% ----------------------------------------------------------------------------------------------------------------- %
+launch :- assert(runs(0)), play.
+    
+% ----------------------------------------------------------------------------------------------------------------- %
+% Export and decide what to do at the end of a game: keep playing, or end it -------------------------------------- %
+% ----------------------------------------------------------------------------------------------------------------- %
+end :- 
+    increment_runs,
+    runs(N),
+    (
+        N < 100 ->
+            play
+        ;
+            true                
+    ).
+
+% ----------------------------------------------------------------------------------------------------------------- %
+% Initialise a game by resetting global variables, generating a word, and launching guesses ----------------------- %
+% ----------------------------------------------------------------------------------------------------------------- %
+play :- 
     retractall(generatedWord(_)), retractall(guessCount(_)), 
     retractall(orangeLetter(_, _)), retractall(greenLetter(_, _)), retractall(greyLetter(_)),
     retractall(wordScore(_, _)), retractall(guessWords(_)),
@@ -28,16 +61,11 @@ launch :-
     assert(guessCount(0)),
     generationWords(GenerationWords), random_member(GeneratedWord, GenerationWords), !, assert(generatedWord(GeneratedWord)),
     write('-----'), nl, write(GeneratedWord), nl, write('-----'), nl,
-    guess.
+    guess, !,
+    end.
 
 % ----------------------------------------------------------------------------------------------------------------- %
-% Update global variables predicates
-% ----------------------------------------------------------------------------------------------------------------- %
-increment_count :- guessCount(Count), retractall(guessCount(_)), NewCount is Count + 1, assert(guessCount(NewCount)).
-update_possible_guesses_list(List) :- retractall(guessWords(_)), assert(guessWords(List)).
-
-% ----------------------------------------------------------------------------------------------------------------- %
-% Sort words by score
+% Sort words by score --------------------------------------------------------------------------------------------- %
 % ----------------------------------------------------------------------------------------------------------------- %
 sort_words_by_score(Words, SortedWords) :-
     map_list_to_pairs(wordScore, Words, Pairs),
@@ -46,7 +74,7 @@ sort_words_by_score(Words, SortedWords) :-
     reverse(SortedWords1, SortedWords).
 
 % ----------------------------------------------------------------------------------------------------------------- %
-% Choose a word from list of possible words, increment count, and evaluate the chosen word
+% Choose a word from list of possible words, increment count, and evaluate the chosen word ------------------------ %
 % ----------------------------------------------------------------------------------------------------------------- %
 guess :-
     guessWords(PossibleWords), 
@@ -60,129 +88,100 @@ guess :-
             nth0(0, SortedPossibleWords, GuessWord)
     ),
     increment_count,
-    evaluate_guess(GuessWord, GeneratedWord, Status),
     write(GuessWord), nl,
-    (Status == unfinished -> guess ; guessCount(C), write(C), nl).
+    evaluate_guess(GuessWord, GeneratedWord, Status),
+    (Status == unfinished -> guess ; true).
 
 % ----------------------------------------------------------------------------------------------------------------- %
-% Check if guess word is the same as generated word, otherwise compute differences and guess again
+% Check if guess word is the same as generated word, otherwise update hint database and word scores --------------- %
 % ----------------------------------------------------------------------------------------------------------------- %
 evaluate_guess(GuessWord, GuessWord, finished).
 evaluate_guess(GuessWord, GeneratedWord, unfinished) :-
     GuessWord \== GeneratedWord,
     atom_string(GuessWord, GuessWS), string_to_list(GuessWS, GuessWordList),
     atom_string(GeneratedWord, GeneratedWS), string_to_list(GeneratedWS, GeneratedWordList),
-    update_orange_letters(GuessWordList, GeneratedWordList, 0, GeneratedWordList),
-    update_green_letters(GuessWordList, GeneratedWordList, 0, GeneratedWordList),
-    update_grey_letters(GuessWordList, GeneratedWordList),
+    update_orange_hints(GuessWordList, GeneratedWordList, 0, GeneratedWordList),
+    update_green_hints(GuessWordList, GeneratedWordList, 0, GeneratedWordList),
+    update_grey_hints(GuessWordList, GeneratedWordList),
     update_word_scores.
 
 % ----------------------------------------------------------------------------------------------------------------- %
-% Update orange hint letter database based on the comparison between the guess and the generated word
+% Update orange hint letter database based on the comparison between the guess and the generated word ------------- %
 % ----------------------------------------------------------------------------------------------------------------- %
-update_orange_letters([X], [X], _, _).
-update_orange_letters([X], [Y], I, FullWord) :- 
+update_orange_hints([X], [X], _, _).
+update_orange_hints([X], [Y], I, FullWord) :- 
     X \== Y, 
-    (member(X, FullWord) -> assert_once(orangeLetter(X, I)); true).
-update_orange_letters([X|R1], [X|R2], I, FullWord) :- 
+    (member(X, FullWord) -> assert_once(orangeLetter(X, I)) ; true).
+update_orange_hints([X|R1], [X|R2], I, FullWord) :- 
     NextI is I + 1, 
-    update_orange_letters(R1, R2, NextI, FullWord).
-update_orange_letters([X|R1], [Y|R2], I, FullWord) :- 
+    update_orange_hints(R1, R2, NextI, FullWord).
+update_orange_hints([X|R1], [Y|R2], I, FullWord) :- 
     X \== Y, 
-    (member(X, FullWord) -> assert_once(orangeLetter(X, I)); true),  
+    (member(X, FullWord) -> assert_once(orangeLetter(X, I)) ; true),  
     NextI is I + 1, 
-    update_orange_letters(R1, R2, NextI, FullWord).
-
+    update_orange_hints(R1, R2, NextI, FullWord).
 
 % ----------------------------------------------------------------------------------------------------------------- %
-% Update green hint letter database based on the comparison between the guess and the generated word
+% Update green hint letter database based on the comparison between the guess and the generated word -------------- %
 % ----------------------------------------------------------------------------------------------------------------- %
-update_green_letters([X], [X], I, _) :- 
+update_green_hints([X], [X], I, _) :- 
     assert_once(greenLetter(X, I)).
-update_green_letters([X], [Y], _, _) :- 
+update_green_hints([X], [Y], _, _) :- 
     X \== Y.
-update_green_letters([X|R1], [X|R2], I, FullWord) :- 
+update_green_hints([X|R1], [X|R2], I, FullWord) :- 
     assert_once(greenLetter(X, I)), 
     NextI is I + 1, 
-    update_green_letters(R1, R2, NextI, FullWord).
-update_green_letters([X|R1], [Y|R2], I, FullWord) :- 
+    update_green_hints(R1, R2, NextI, FullWord).
+update_green_hints([X|R1], [Y|R2], I, FullWord) :- 
     X \== Y, 
     NextI is I + 1, 
-    update_green_letters(R1, R2, NextI, FullWord).
+    update_green_hints(R1, R2, NextI, FullWord).
     
 % ----------------------------------------------------------------------------------------------------------------- %
-% Update grey hint letter database based on the comparison between the guess and the generated word
+% Update grey hint letter database based on the comparison between the guess and the generated word --------------- %
 % ----------------------------------------------------------------------------------------------------------------- %
-update_grey_letters(GuessWordList, GeneratedWordList) :-
+update_grey_hints(GuessWordList, GeneratedWordList) :-
     list_to_set(GuessWordList, GuessWordSet),
     subtract(GuessWordSet, GeneratedWordList, GreyLetters),
-    update_grey_letters_1(GreyLetters).
-update_grey_letters_1([]).
-update_grey_letters_1([X|R]) :-
+    update_grey_hints_1(GreyLetters).
+update_grey_hints_1([]).
+update_grey_hints_1([X|R]) :-
     assert_once(greyLetter(X)), 
-    update_grey_letters_1(R).
+    update_grey_hints_1(R).
 
 % ----------------------------------------------------------------------------------------------------------------- %
-% Update and assert scores for every possible word
+% Update and assert scores for every possible word ---------------------------------------------------------------- %
 % ----------------------------------------------------------------------------------------------------------------- %
 update_word_scores :-
     retractall(wordScore(_, _)), 
     guessWords(PossibleWords), 
     update_word_scores_1(PossibleWords),
-    exclude([X]>>(wordScore(X, L), L < 0), PossibleWords, FilteredPossibleWords),
-    update_possible_guesses_list(FilteredPossibleWords).
+    findall(Word, isRelevantWord(Word), GuessWords),
+    update_possible_guesses_list(GuessWords).
 update_word_scores_1([]).
 update_word_scores_1([Word|Rest]) :-
-    evaluate_orange_score(Word, OrangeScore),
-    evaluate_green_score(Word, GreenScore),
-    evaluate_grey_score(Word, GreyScore),
-    wordFrequency(Word, Frequency),
-    Score is GreenScore + OrangeScore + GreyScore + 0.5*Frequency,
-    assert(wordScore(Word, Score)),
+    atom_string(Word, WS), string_to_list(WS, WordList),
+    computer_letter_score(WordList, 0, 0, LetterScore, Keep),
+    (
+        Keep == keep -> 
+            wordFrequency(Word, Frequency),
+            Score is LetterScore + 0.5*Frequency, 
+            assert(wordScore(Word, Score)) 
+        ; 
+            true
+    ),
     update_word_scores_1(Rest).
 
 % ----------------------------------------------------------------------------------------------------------------- %
-% Evaluate orange score
+% Compute letter score for a given score -------------------------------------------------------------------------- %
 % ----------------------------------------------------------------------------------------------------------------- %
-has_correct_orange(Letter, WordList) :- 
-    member(Letter, WordList), 
-    nth0(I1, WordList, Letter), 
-    orangeLetter(Letter, I2),
-    I1 \== I2.
-has_incorrect_orange(Letter, WordList) :- 
-    member(Letter, WordList), 
-    nth0(I, WordList, Letter), 
+computer_letter_score([], _, Score, Score, keep).
+computer_letter_score([Letter|_], I, _, _, nokeep) :-
     orangeLetter(Letter, I).
-evaluate_orange_score(Word, Score) :-
-    atom_string(Word, WS), string_to_list(WS, WordList), list_to_set(WordList, WordSet),
-    findall(L, has_correct_orange(L, WordSet), CorrectOrangeLetters),
-    findall(L, has_incorrect_orange(L, WordList), IncorrectOrangeLetters),
-    length(CorrectOrangeLetters, PlusScore),
-    length(IncorrectOrangeLetters, MinusScore),
-    Score is PlusScore - 100 * MinusScore.
-
-% ----------------------------------------------------------------------------------------------------------------- %
-% Evaluate green score
-% ----------------------------------------------------------------------------------------------------------------- %
-has_correct_green(Letter, WordList) :- 
-    member(Letter, WordList), 
-    nth0(I, WordList, Letter), 
-    greenLetter(Letter, I).
-evaluate_green_score(Word, Score) :-
-    atom_string(Word, WS), string_to_list(WS, WordList), list_to_set(WordList, WordSet),
-    findall(L, has_correct_green(L, WordSet), GreenLetters),
-    length(GreenLetters, PlusScore),
-    Score is 2 * PlusScore.
-
-% ----------------------------------------------------------------------------------------------------------------- %
-% Evaluate grey score
-% ----------------------------------------------------------------------------------------------------------------- %
-has_incorrect_grey(Letter, WordList) :-
-    member(Letter, WordList),
+computer_letter_score([Letter|_], _, _, _, nokeep) :-
     greyLetter(Letter).
-evaluate_grey_score(Word, Score) :-
-    atom_string(Word, WS), string_to_list(WS, WordList),
-    findall(L, has_incorrect_grey(L, WordList), GreyLetters),
-    length(GreyLetters, MinusScore),
-    Score is -100 * MinusScore.
-    
+computer_letter_score([Letter|Rest], I, Score, NextScore, Keep) :-
+    (greenLetter(Letter, I) -> NScore is Score + 2 ; NScore is Score),
+    (orangeLetter(Letter, P), I \== P -> NNScore is NScore + 1 ; NNScore is NScore),
+    NextI is I + 1,
+    computer_letter_score(Rest, NextI, NNScore, NextScore, Keep).
